a4776ce5302a223c7b5b9f090472c19610c9cee8
김관진 <cozyschooll@gmail.com>
2024-03-09 03:08:40 +0900

    Test: ProductService Test code 구현

--- src/test/java/service/ProductServiceTest.java
+++ src/test/java/service/ProductServiceTest.java
@@ -2,11 +2,25 @@ package service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import adapter.ProductAdapter;
+import dto.res.ProductResponseDto;
+import exception.ProductNotFoundException;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.Mock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@QuarkusTest
 class ProductServiceTest {
+
+    @Inject
+    ProductService productService;
 
     @Test
     void getProductById() {
+        Assertions.assertThrows(ProductNotFoundException.class, () -> productService.getProductById(0L));
     }
 }
